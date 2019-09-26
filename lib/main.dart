090cdcb464c7b30c725cc2fa9be279d272a87873
hanhuizhu <hanhuizhuxyl@gmail.com>
2019-09-26 08:29:39 +0800

    add swiper

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
 
 void main() => runApp(MyApp());
 
@@ -91,6 +92,32 @@ class _MyHomePageState extends State<MyHomePage> {
                       ),
                     )
                     .toList(),
+              ),
+            ),
+            Container(
+              height: 140,
+              child: new Swiper(
+                itemBuilder: (BuildContext context, int index) {
+                  const list = [
+                    'http://familyweb.oss-cn-shanghai.aliyuncs.com/zongheng/home/banner/banner-1.jpg',
+                    'http://familyweb.oss-cn-shanghai.aliyuncs.com/zongheng/home/banner/banner-2.jpg',
+                    'http://familyweb.oss-cn-shanghai.aliyuncs.com/zongheng/home/banner/banner-3.jpg',
+                    'http://familyweb.oss-cn-shanghai.aliyuncs.com/zongheng/home/banner/banner-4.jpg',
+                  ];
+                  return new Image.network(
+                    list[index],
+                    fit: BoxFit.fill,
+                  );
+                },
+                itemCount: 3,
+                itemHeight: 140,
+                pagination: new SwiperPagination(
+                  alignment: Alignment.bottomRight,
+                  builder: DotSwiperPaginationBuilder(
+                    color: Colors.black54,
+                    activeColor: Color(0XFFe95e57),
+                  ),
+                ),
               ),
             ),
           ],
