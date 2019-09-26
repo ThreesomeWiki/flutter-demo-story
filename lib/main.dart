c68b9e06ff8dc117a365559915c61180ccaf6c7c
hanhuizhu <hanhuizhuxyl@gmail.com>
2019-09-26 12:32:42 +0800

    update home sec navs

--- lib/main.dart
+++ lib/main.dart
@@ -141,6 +141,50 @@ class _MyHomePageState extends State<MyHomePage> {
                       color: Color(0xff7297c1),
                       borderRadius: BorderRadius.circular(5),
                     ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        Image.network(
+                          'http://familyweb.oss-cn-shanghai.aliyuncs.com/zongheng/icon/book-white.png',
+                          width: 25,
+                          height: 25,
+                        ),
+                        Text(
+                          '免费专区',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 12,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    width: 165,
+                    height: 40,
+                    decoration: BoxDecoration(
+                      color: Color(0xffd4b8d6),
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        Image.network(
+                          'http://familyweb.oss-cn-shanghai.aliyuncs.com/zongheng/icon/shoe-white.png',
+                          width: 25,
+                          height: 25,
+                        ),
+                        Text(
+                          '女频精选',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 12,
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ],
               ),
