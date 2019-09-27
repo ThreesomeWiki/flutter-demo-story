e8e97c3db123853222af2aa68beb2224fd0537b1
hanhuizhu <hanhuizhuxyl@gmail.com>
2019-09-27 14:57:17 +0800

    beauty project struct

--- lib/main.dart
+++ lib/main.dart
@@ -1,68 +1,11 @@
 import 'package:flutter/material.dart';
-import './components/Navs.dart' as Navs;
-import './components/Header.dart' as Header;
-import './view/home/Home.dart' as Home;
-import './view/category/Category.dart' as Category;
-import './view/rank/Rank.dart' as Rank;
+import './view/MainScreen.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: MyHomePage());
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  String activeNav = '排行';
-  // 修改NAV
-  changeNavHandle(v) {
-    setState(() {
-      activeNav = v;
-    });
-  }
-
-  // 渲染
-  render() {
-    if (activeNav == '首页') {
-      return Home.Home();
-    } else if (activeNav == '分类') {
-      return Category.Category();
-    } else if (activeNav == '排行') {
-      return Rank.Rank();
-    }
-    return Container();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: Size.fromHeight(44.0),
-        child: AppBar(
-            backgroundColor: Color(0XFFF5F5F5),
-            elevation: 0,
-            title: Header.Header(
-              title: '纵横中文网',
-            )),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            Navs.Navs(
-              activeNav: activeNav,
-              onChange: changeNavHandle,
-            ),
-            render(),
-          ],
-        ),
-      ),
-    );
+    return MaterialApp(home: MainScreen());
   }
 }
---
+++ lib/view/MainScreen.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import '../components/Navs.dart' as Navs;
+import '../components/Header.dart' as Header;
+import './home/Home.dart' as Home;
+import './category/Category.dart' as Category;
+import './rank/Rank.dart' as Rank;
+
+class MainScreen extends StatefulWidget {
+  static String pageName = '主页';
+  @override
+  _MainScreen createState() => _MainScreen();
+}
+
+class _MainScreen extends State<MainScreen> {
+  String activeNav = '排行';
+  // 修改NAV
+  changeNavHandle(v) {
+    setState(() {
+      activeNav = v;
+    });
+  }
+
+  // 渲染
+  render() {
+    if (activeNav == '首页') {
+      return Home.Home();
+    } else if (activeNav == '分类') {
+      return Category.Category();
+    } else if (activeNav == '排行') {
+      return Rank.Rank();
+    }
+    return Container();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(44.0),
+        child: AppBar(
+            backgroundColor: Color(0XFFF5F5F5),
+            elevation: 0,
+            title: Header.Header(
+              title: '纵横中文网',
+            )),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Navs.Navs(
+              activeNav: activeNav,
+              onChange: changeNavHandle,
+            ),
+            render(),
+          ],
+        ),
+      ),
+    );
+  }
+}
