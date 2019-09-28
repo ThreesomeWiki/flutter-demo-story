aa6d0913a53ce44906fd9472d09b4b3de6b12dfa
hanhuizhu <hanhuizhuxyl@gmail.com>
2019-09-28 09:58:57 +0800

    add turn to

--- lib/view/home/components/HomeItemRow.dart
+++ lib/view/home/components/HomeItemRow.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../../book/BookDetail.dart' as BookDetail;
 
 class HomeItemRow extends StatefulWidget {
   static String pageName = '首页卡片2';
@@ -11,33 +12,40 @@ class HomeItemRow extends StatefulWidget {
 class _HomeItemRow extends State<HomeItemRow> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 324,
-      height: 46,
-      decoration: BoxDecoration(
-        border: Border(
-          bottom: BorderSide(
-            width: 1,
-            color: Color(0xffe6e6e6),
-          ),
-        ),
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: <Widget>[
-          Text(
-            widget.item['title'],
-            style: TextStyle(
-              color: Color(0xff3333333),
-              fontSize: 15,
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(context, MaterialPageRoute(builder: (context) {
+          return BookDetail.BookDetail();
+        }));
+      },
+      child: Container(
+        width: 324,
+        height: 46,
+        decoration: BoxDecoration(
+          border: Border(
+            bottom: BorderSide(
+              width: 1,
+              color: Color(0xffe6e6e6),
             ),
           ),
-          Image.network(
-            'http://familyweb.oss-cn-shanghai.aliyuncs.com/zongheng/icon/right.png',
-            width: 20,
-            height: 20,
-          ),
-        ],
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Text(
+              widget.item['title'],
+              style: TextStyle(
+                color: Color(0xff3333333),
+                fontSize: 15,
+              ),
+            ),
+            Image.network(
+              'http://familyweb.oss-cn-shanghai.aliyuncs.com/zongheng/icon/right.png',
+              width: 20,
+              height: 20,
+            ),
+          ],
+        ),
       ),
     );
   }
